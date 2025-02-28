b8c991f0f049ceb9841b8538494d0da9bb3973c7
jack-g-coms <jackgoeders2005@gmail.com>
2025-02-28 13:32:58 -0600

    Config

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate, Outlet} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route, Navigate, Outlet} from 'react-router-dom';
 
 import AuthContext from './js/modules/AuthContext';
 
